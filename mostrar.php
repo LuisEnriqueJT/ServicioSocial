c6e7c9d2a5d39ff9a1380f99ab7fb6c6d374a3af
LuisEnriqueJT <luisenrique.jt@outlook.com>
2019-11-02 16:09:51 -0600

    Se agrego el meta viewport

--- mostrar.php
+++ mostrar.php
@@ -10,6 +10,7 @@
 	<title>Index</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/main.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 </head>
 <body>
 
